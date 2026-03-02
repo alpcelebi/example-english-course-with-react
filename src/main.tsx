30d196a9c44f96718841eac9855d6aacf375d87a
alpcelebi <alpcelebi33@gmail.com>
2026-03-03 00:57:32 +0300

    fix: add BrowserRouter basename for GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/example-english-course-with-react">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
